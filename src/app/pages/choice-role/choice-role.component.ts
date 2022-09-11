f8d7faa67150f33fba731951fe1de74130c17322
amirbenahmed <benahmedamir@hotmail.fr>
2022-09-11 20:12:02 +0100

    href

--- src/app/pages/choice-role/choice-role.component.ts
+++ src/app/pages/choice-role/choice-role.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-choice-role',
   templateUrl: './choice-role.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoiceRoleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  goTo(){
+    this.router.navigate(['shop-login']);
+  }
 }
